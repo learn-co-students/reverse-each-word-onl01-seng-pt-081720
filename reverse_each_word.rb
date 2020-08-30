97fe081ea0deba941922804fe038d756d32fa922
Myles Janik <mylesjanik@gmail.com>
2020-08-30 23:01:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,12 @@
 def reverse_each_word(string)
+  new_string = ""
   new_array = string.split(" ")
+  counter = new_array.length
   
+  while counter >= 0
+    new_string << new_array[counter]
+    counter -= 1
+  end
   
+  puts new_string.reverse
 end
